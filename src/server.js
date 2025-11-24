4ee710490abdfc37e6a450bea3e0c2365d053140
Washington <washington.contato1327@gmail.com>
2025-11-23 21:46:00 -0300

    Ajusta configuração do Docker, Prisma e aplicação. Corrige variáveis, schema e inicialização.

--- src/server.js
+++ src/server.js
@@ -2,10 +2,6 @@ import app from "./app.js";
 
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
-
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Servidor rodando na porta ${port}`);
 });
